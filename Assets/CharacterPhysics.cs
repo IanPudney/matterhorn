7ff6ec25ab00108d38e5e45cdb24d58f102a74fd
ddrocco <keredoccor@gmail.com>
2015-03-28 04:29:36 -0400

    First push!!!!!!

---
+++ Assets/CharacterPhysics.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterPhysics : MonoBehaviour {
+	public List<GravityWell> GravitationalBodies;
+	Rigidbody characterRigidbody;
+	
+	void Start () {
+		if (characterRigidbody == null) {
+			characterRigidbody = GetComponent<Rigidbody>();
+		}
+		characterRigidbody.AddForce(150f * Vector3.right);
+	}
+	
+	void FixedUpdate () {
+		UpdateTrajectory();
+	}
+	
+	public void AddWell(GravityWell well) {
+		GravitationalBodies.Add(well);
+	}
+	
+	public void RemoveWell(GravityWell well) {
+		GravitationalBodies.Remove(well);
+	}
+	
+	void UpdateTrajectory() {
+		foreach(GravityWell well in GravitationalBodies) {
+			GetComponent<Rigidbody>().AddForce(GetForce(well));
+		}
+	}
+	
+	Vector3 GetForce(GravityWell well) {
+		float distance = Vector3.Distance(well.transform.position, transform.position);
+		Vector2 direction = (well.transform.position - transform.position).normalized;
+		return direction * well.mass * GravityWell.gravitationalConstant / Mathf.Pow (distance, 2);
+	}
+}
---
+++ Assets/StateControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StateControl : MonoBehaviour {
+	public static StateControl main;
+	public enum State {
+		drawing,
+		launching
+	};
+	public State desiredState;
+	public bool toggleEnabled;
+	public static State state;
+	
+	public float magneticPower = -0.5f;
+	public Text magneticDisplay;
+	
+	void Start () {
+		main = this;
+	}
+	
+	void Update() {
+		if (toggleEnabled) {
+			toggleEnabled = false;
+			state = desiredState;
+		}
+		
+		UpdateDisplay();
+	}
+	
+	void UpdateDisplay() {
+		if (magneticDisplay == null) {
+			magneticDisplay = GameObject.FindGameObjectWithTag("MagneticConstantDisplay").GetComponent<Text>();
+			if (magneticDisplay == null) {
+				return	;
+			}	
+		}
+		magneticDisplay.text = magneticPower.ToString("F4");
+	}
+}
