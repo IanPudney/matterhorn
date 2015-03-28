b55cf33967008a95ec2ed3cf1ec42e9a1abe584b
Ian Pudney <ian.pudney.git@gmail.com>
2015-03-28 13:22:05 -0400

    System now permits customization of SlidingWall as well

--- Assets/Scripts/SlidingWallMove.cs
+++ Assets/Scripts/SlidingWallMove.cs
@@ -6,7 +6,10 @@ public class SlidingWallMove : MonoBehaviour {
 	public GameObject wall;
 	public GameObject endpieceOne;
 	public GameObject endpieceTwo;
-	public float velocity;
+	public float velocity = 3f;
+	public float height = 2f;
+	public float travel = 20f;
+	public float startPosition = 5f;
 
 	private Vector3 velocityVector;
 
@@ -17,7 +20,40 @@ public class SlidingWallMove : MonoBehaviour {
 		if (wallRigidBody == null) {
 			wallRigidBody = wall.GetComponent<Rigidbody>();
 		}
+		setHeight (height);
+		setTravel (travel);
+		setStartPosition (startPosition);
+	}
+
+	void OnGameStart() {
 		velocityVector = new Vector3 (0, velocity, 0);
+	}
+
+	void setHeight(float height) {
+		Transform[] transforms = GetComponentsInChildren<Transform> ();
+		for (int i=0; i<transforms.Length; ++i) {
+			if (transforms[i].name == "SlidingWall") {
+				transforms[i].localScale = new Vector3(1f, height, 1f);
+			}
+		}
+	}
+
+	void setTravel(float travel) {
+		Transform[] transforms = GetComponentsInChildren<Transform> ();
+		for (int i=0; i<transforms.Length; ++i) {
+			if (transforms[i].name == "EndpieceOne") {
+				transforms[i].localPosition = new Vector3(0f, travel, 0f);
+			}
+		}
+	}
+
+	void setStartPosition(float startPosition) {
+		Transform[] transforms = GetComponentsInChildren<Transform> ();
+		for (int i=0; i<transforms.Length; ++i) {
+			if (transforms[i].name == "SlidingWall") {
+				transforms[i].localPosition = new Vector3(0f, startPosition, 0f);
+			}
+		}
 	}
 	
 	// Update is called once per frame
