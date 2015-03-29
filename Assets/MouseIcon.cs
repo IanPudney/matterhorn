4927bdc6794f5255ceccf68cb275f174bce3ba30
ddrocco <keredoccor@gmail.com>
2015-03-29 00:31:29 -0400

    Brought levels up to speed

--- Assets/MouseIcon.cs
+++ Assets/MouseIcon.cs
@@ -1,12 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MouseIcon : MonoBehaviour {
 	void OnGameStart() {
-		gameObject.SetActive(false);
+		SetRendererAndCollider(gameObject, false);
 	}
 	
 	void RestoreState() {
-		gameObject.SetActive(true);
+		SetRendererAndCollider(gameObject, true);
+	}
+	
+	protected void SetRendererAndCollider(GameObject gameObject, bool toggle) {
+		if (gameObject.GetComponent<Renderer>()) {
+			gameObject.GetComponent<Renderer>().enabled = toggle;
+		}
+		if (gameObject.GetComponent<Image>()) {
+			gameObject.GetComponent<Image>().enabled = toggle;
+		}
+		if (gameObject.GetComponent<Text>()) {
+			gameObject.GetComponent<Text>().enabled = toggle;
+		}
+		
+		if (gameObject.GetComponent<Collider>()) {
+			gameObject.GetComponent<Collider>().enabled = toggle;
+		}
+		
+		foreach (Transform child in gameObject.transform) {
+			SetRendererAndCollider(child.gameObject, toggle);
+		}
 	}
 }
