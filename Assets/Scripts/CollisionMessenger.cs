1810ebf4b807e60a7e83c708ccb89c1762e83fc1
ddrocco <keredoccor@gmail.com>
2015-03-29 08:17:11 -0400

    Got rid of some logs

--- Assets/Scripts/CollisionMessenger.cs
+++ Assets/Scripts/CollisionMessenger.cs
@@ -18,9 +18,7 @@ public class CollisionMessenger : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter(Collision collision) {
-		Debug.Log ("Collision Detected On Stopper");
 		if (collision.gameObject == target1 || collision.gameObject == target2) {
-			Debug.Log ("Collision Matches");
 			recipient.SendMessage("ReverseDirection");
 		}
 	}
